b3c8c0681563c766b6251877e33f9f2a5111ecae
Tyuiu.OsadetsAA@outlook.com <Tyuiu.OsadetsAA@outlook.com>
2024-11-27 04:16:48 +0500

    Task4

--- Tyuiu.OsadetsAA.Sprint5.Task4.V20.Lib/DataService.cs
+++ Tyuiu.OsadetsAA.Sprint5.Task4.V20.Lib/DataService.cs
@@ -7,7 +7,8 @@ namespace Tyuiu.OsadetsAA.Sprint5.Task4.V20.Lib
         public double LoadFromDataFile(string path)
         {
             string strX = File.ReadAllText(path);
-            double x = double.Parse(strX);
+            string s = s.Replace(",", ".");
+            double x = double.Parse(s);
             double res = Math.Round(Math.Pow(((Math.Pow(x, 2)) / (Math.Sin(x))), 3), 3);
             return res;
 
