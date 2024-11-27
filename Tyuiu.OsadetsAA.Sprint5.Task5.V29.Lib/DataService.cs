ba37a10c117a840434bcd9af5e19a2a5c5f04e77
Tyuiu.OsadetsAA@outlook.com <Tyuiu.OsadetsAA@outlook.com>
2024-11-27 20:02:28 +0500

    Task5

--- Tyuiu.OsadetsAA.Sprint5.Task5.V29.Lib/DataService.cs
+++ Tyuiu.OsadetsAA.Sprint5.Task5.V29.Lib/DataService.cs
@@ -7,23 +7,19 @@ namespace Tyuiu.OsadetsAA.Sprint5.Task5.V29.Lib
     {
         public double LoadFromDataFile(string path)
         {
-            double res = 0;
-            using (StreamReader reader = new StreamReader(path))
-            {
-                string[] lines = File.ReadAllLines(path);
+            double res = 11;
+
+            string[] lines = File.ReadAllLines(path);
                 
-                foreach (string line in lines)
+            foreach (string line in lines)
+            {
+                if (double.TryParse(line, NumberStyles.Float, CultureInfo.InvariantCulture, out double number))
                 {
-                    if (double.TryParse(line, NumberStyles.Float, CultureInfo.InvariantCulture, out double number))
-                    {
-                        if (number == 11)
-                        {
-                            res += number;
-                        }
-                    }
+                    if (number == 11)
+                        res += number;
                 }
-                return res;
             }
+            return res;
         }
     }
 }
